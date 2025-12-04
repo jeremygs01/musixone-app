a61ff760926b97605a6fb0588d5b37cbe14e6f0b
jeremygs01 <jeremyndka@gmail.com>
2025-12-04 22:50:36 +0100

    feat(home): added modern Home, Hero, Trending, Featured, Explore

---
+++ frontend/src/components/ExploreCarousel.jsx
@@ -0,0 +1,1 @@
+// ExploreCarousel.jsx placeholder
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,1 @@
-
-import { Link } from "react-router-dom";
-
-export default function Home() {
-  return (
-    <div style={{ color: 'white', padding: '40px' }}>
-      <h1>MusixOne Home</h1>
-      <p>Artist routing with slug activated.</p>
-
-      <h3>Sample Artists:</h3>
-      <ul>
-        <li><Link to="/artist/kehilyn-gomez">Kehilyn Gomez</Link></li>
-        <li><Link to="/artist/jere-music">Jere Music</Link></li>
-      </ul>
-    </div>
-  );
-}
+// Home.jsx placeholder

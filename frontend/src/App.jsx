35371a50cb6fc8c616dc131f2e96e23288a13176
jeremygs01 <jeremyndka@gmail.com>
2025-12-04 21:23:10 +0100

    Fix: added BrowserRouter to React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ArtistProfile from "./pages/ArtistProfile";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/artist/:slug" element={<ArtistProfile />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/artist/:slug" element={<ArtistProfile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
